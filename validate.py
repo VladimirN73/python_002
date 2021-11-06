550d8e0132c31c61eb0c58194c5ba97dc4264364
Vladimir Nalivaika <vladimir.nalivaika@bridging-it.de>
2021-11-06 19:28:21 +0100

    simplify

--- validate.py
+++ validate.py
@@ -6,7 +6,7 @@ import glob
 import zipfile
 
 
-def create_weights():
+def unzip_file():
     #
     zipFile = "voc.zip"
     zips = glob.glob('vox.zip.*')
@@ -15,9 +15,8 @@ def create_weights():
             with open(source, "rb") as z:
                 f.write(z.read())
 
-    # this fix is not needed on Windows and does not work on unix ...
+    # this fix is not needed on Windows and it does not work on unix ...
     fix_zip(zipFile)
-    # fix_zip_2(zipFile)
 
     with zipfile.ZipFile(zipFile) as zf:
         zf.extractall()
@@ -34,7 +33,7 @@ def fix_zip(zipFile):
     # Finding the end of the central directory:
     #   http://stackoverflow.com/questions/8593904/how-to-find-the-position-of-central-directory-in-a-zip-file
     #   http://stackoverflow.com/questions/20276105/why-cant-python-execute-a-zip-archive-passed-via-stdin
-    # This second link is only losely related, but echos the first,
+    # This second link is only loosely related, but echos the first,
     # "processing a ZIP archive often requires backwards seeking"
 
     with open(zipFile, "r+b") as f:
@@ -42,64 +41,24 @@ def fix_zip(zipFile):
         # reverse find: this string of bytes is the end of the zip's central directory.
         pos = content.rfind(b'\x50\x4b\x05\x06')
         if pos > 0:
-            f.seek(pos + 20)  # +20: see secion V.I in 'ZIP format' link above.
+            f.seek(pos + 20)
             f.truncate()
             f.write(b'\x00\x00')  # Zip file comment length: 0 byte length; tell zip applications to stop reading.
 
 
-def fix_zip_2(zipFile):
-    with open(zipFile, "r+b") as f:
-        content = f.read()
-        pos = content.rfind(b'\x50\x4b\x05\x06')
-        if pos > 0:
-            f.seek(pos + 22)
-            f.truncate()
-
-
-def fix_zip_show_checkpoints(zipFile):
-    # https://python-list.python.narkive.com/BkZgCEiq/badzipfile-file-is-not-a-zip-file
-    # import sys
-    points = [
-        # ("FileHeader", b'\x03\x04'),  # magic number for file header
-        # ("DataDescriptor", b'\x07\x08'),  # see PKZIP APPNOTE (V) (C)
-        # ("CentralDir", b'\x01\x02'),  # magic number for central  directory
-        # ("EndArchive", b'\x05\x06'),  # magic number for end of archive record
-        # ("EndArchive64", b'\x06\x06'),  # magic token for Zip64 header
-        # ("EndArchive64Locator", b'\x06\x07'),  # magic token for locator header
-        # ("ArchiveExtraData", b'PK\x06\x08'),  # APPNOTE (V) (E)
-        # ("DigitalSignature", b'\x05\x05'),  # APPNOTE (V) (F)
-        ("End of CentralDir", b'\x50\x4b\x05\x06')
-    ]
-    with open(zipFile, "rb") as f:
-        buff = f.read()
-        f.close()
-        blen = len(buff)
-        print("archive size is", blen)
-        for point_name, point_value in points:
-            print("search for", point_name, point_value)
-            pos = 0
-            while pos < blen:
-                pos = buff.find(point_value, pos)
-                if pos < 0:
-                    break
-                print("%s at %d" % (point_name, pos))
-                pos += 4
-
-
-def run():
-    #
-    file_target = 'voc.pth'
+def delete_file(filename):
+    file_target = filename
     flag = os.path.isfile(file_target)
     if flag:
         os.remove(file_target)
 
-    file_zip = 'voc.zip'
-    flag = os.path.isfile(file_zip)
-    if flag:
-        os.remove(file_zip)
 
-    create_weights()
+def run():
+    delete_file('voc.pth')
+    delete_file('voc.zip')
+    unzip_file()
 
 
 if __name__ == '__main__':
     run()
+
